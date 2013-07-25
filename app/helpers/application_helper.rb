c30117259308169a0dd1ee9eb7087ced2090862b
jinwen <jinwen@factual.com>
2013-07-25 15:57:54 +0800

    add full_title in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(title)
+    base_title = "Rails Sample Application" 
+    if title.empty?
+      base_title
+    else
+      "#{base_title} | #{title}"
+    end
+  end
 end
