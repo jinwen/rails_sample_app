6f108a538427abfb335c91a40a86e73fdb0806bd
jinwen <jinwen@factual.com>
2013-11-24 21:01:00 +0800

    change have_content to have_selector

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,7 +6,7 @@ describe "UserPages" do
   describe "signup pages" do
     before { visit signup_path }
 
-    it { should have_content('Sign Up') }
+    it { should have_selector('h1', text: 'Sign Up') }
     it { should have_title(full_title('Sign Up')) }
   end
 end
